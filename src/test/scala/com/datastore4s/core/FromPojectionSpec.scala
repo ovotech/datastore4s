923b199e0d5c7e3124a68c3b7510667bd384f4d9
Oliver Boyle <32272028+olib963@users.noreply.github.com>
2018-02-04 23:37:18 +0000

    Add FromProjection compilation tests and some TODO tasks

---
+++ src/test/scala/com/datastore4s/core/FromPojectionSpec.scala
@@ -0,0 +1,34 @@
+package com.datastore4s.core
+
+import java.time.Instant
+
+import org.scalatest.prop.GeneratorDrivenPropertyChecks
+import org.scalatest.{FeatureSpec, Matchers}
+
+class FromProjectionSpec extends FeatureSpec with Matchers with GeneratorDrivenPropertyChecks {
+
+  feature("The FromProjection macro") {
+    scenario("Attempt to make a FromProjection of a type that is not a case class") {
+      "FromProjection[NonCaseClass]" shouldNot compile
+    }
+    scenario("Attempt to make a FromProjection when an implicit field format is not available") {
+      "FromProjection[MissingKeyFormatContainer]" shouldNot compile
+    }
+    scenario("A simple case class") {
+      // TODO cannot currently make this test since Projection entities cannot be created in the test.
+    }
+  }
+
+  class NonCaseClass(val key: String) extends DatastoreEntity[String]
+
+  case class MissingFieldFormatType()
+
+  case class MissingKeyFormatContainer(key: MissingFieldFormatType)
+
+  case class SomeCustomType(stringField: String,
+                            someLongField: Long,
+                            someIntField: Int,
+                            someBooleanField: Boolean,
+                            someTimeField: Instant)
+
+}
