f66c09739d8810eb23798a25d09e04beb0f4d388
Oliver Boyle <32272028+olib963@users.noreply.github.com>
2018-02-16 16:06:02 +0000

    Remove duplicated tests

--- src/test/scala/com/ovoenergy/datastore4s/FieldFormatSpec.scala
+++ src/test/scala/com/ovoenergy/datastore4s/FieldFormatSpec.scala
@@ -1,118 +1,22 @@
 package com.ovoenergy.datastore4s
 
-import java.time.Instant
 
-import com.google.cloud.datastore.{Blob, LatLng}
 import org.scalacheck.Gen
 import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.prop.GeneratorDrivenPropertyChecks
 import com.ovoenergy.datastore4s.FieldFormat._
-import com.google.cloud.Timestamp
 import com.ovoenergy.datastore4s.internal._
-import com.ovoenergy.datastore4s.internal.ValueFormat.{BigDecimalStringValueFormat, ByteArrayValueFormat, InstantEpochMillisValueFormat}
 import com.ovoenergy.datastore4s.utils.StubEntityBuilder
 
-class FieldFormatSpec extends FlatSpec with FieldFormatTestCases {
+class FieldFormatSpec extends FlatSpec with GeneratorDrivenPropertyChecks with Matchers {
 
-  implicit val instantFormat = InstantEpochMillisValueFormat // TODO just reduce to one simple case
-  implicit val bdFormat = BigDecimalStringValueFormat
-  implicit val baFormat = ByteArrayValueFormat
-
-  "The String format" should "take any string and store it as a field" in {
+  "The implicit value to field format function" should "take any value format and use it to store it as a field" in {
     forallTestRoundTrip(Gen.alphaNumStr)
   }
 
-  it should "store the value as a string field" in {
+  it should "store the value as a string field if using the string value format" in {
     testEntity("value") {
       _.field(fieldName) shouldBe Some(StringValue("value"))
-    }
-  }
-
-  "The Long format" should "take any long and store it as a field" in {
-    forallTestRoundTrip(Gen.choose(Long.MinValue, Long.MaxValue))
-  }
-
-  it should "store the value as a long field" in {
-    testEntity(12L) {
-      _.field(fieldName) shouldBe Some(LongValue(12L))
-    }
-  }
-
-  "The Boolean format" should "take any boolean and store it as a field" in {
-    forallTestRoundTrip(Gen.oneOf(true, false))
-  }
-
-  it should "store the value as a boolean field" in {
-    testEntity(true) {
-      _.field(fieldName) shouldBe Some(BooleanValue(true))
-    }
-  }
-
-  "The Double format" should "take any double and store it as a field" in {
-    forallTestRoundTrip(Gen.choose(Double.MinValue, Double.MaxValue))
-  }
-
-  it should "store the value as a double field" in {
-    testEntity(5.65842126) {
-      _.field(fieldName) shouldBe Some(DoubleValue(5.65842126))
-    }
-  }
-
-  "The Int format" should "take any integer and store it as a field" in {
-    forallTestRoundTrip(Gen.choose(Int.MinValue, Int.MaxValue))
-  }
-
-  it should "store the value as a long field" in {
-    testEntity(10) {
-      _.field(fieldName) shouldBe Some(LongValue(10))
-    }
-  }
-
-  "The Timestamp format" should "take any timestamp and store it as a field" in {
-    forallTestRoundTrip(Gen.choose(0L, 100000L).map(Timestamp.ofTimeMicroseconds(_)))
-  }
-
-  it should "store the value as a blob field" in {
-    val time = Timestamp.now()
-    testEntity(time) { entity =>
-      entity.field(fieldName) shouldBe Some(TimestampValue(time))
-    }
-  }
-
-  "The LatLng format" should "take any latlng and store it as a field" in {
-    val latLngGen = for {
-      lat <- Gen.choose(-90, 90)
-      lang <- Gen.choose(-90, 90)
-    } yield LatLng.of(lat, lang)
-    forallTestRoundTrip(latLngGen)
-  }
-
-  it should "store the value as a blob field" in {
-    val latLang = LatLng.of(12, 109.2)
-    testEntity(latLang) { entity =>
-      entity.field(fieldName) shouldBe Some(LatLngValue(latLang))
-    }
-  }
-
-  "The EpochMilli format" should "take any Instant and store it as a field" in {
-    forallTestRoundTrip(Gen.choose(Long.MinValue, Long.MaxValue).map(Instant.ofEpochMilli(_)))
-  }
-
-  it should "store the value as a long field" in {
-    val millis = 1236785472L
-    testEntity(Instant.ofEpochMilli(millis)) { entity =>
-      entity.field(fieldName) shouldBe Some(LongValue(millis))
-    }
-  }
-
-  "The BigDecimalString format" should "take any BigDecimal and store it as a field" in {
-    forallTestRoundTrip(Gen.choose(Double.MinValue, Double.MaxValue).map(BigDecimal(_)))
-  }
-
-  it should "store the value as a string field" in {
-    val amount = 601623.873
-    testEntity(BigDecimal(amount)) { entity =>
-      entity.field(fieldName) shouldBe Some(StringValue("601623.873"))
     }
   }
 
@@ -141,13 +45,9 @@ class FieldFormatSpec extends FlatSpec with FieldFormatTestCases {
     }
   }
 
-}
-
-trait FieldFormatTestCases extends GeneratorDrivenPropertyChecks with Matchers {
-
   val fieldName = "FIELD"
 
-  def forallTestRoundTrip[A](generator: Gen[A])(implicit fieldFormat: FieldFormat[A]) = {
+  private def forallTestRoundTrip[A](generator: Gen[A])(implicit fieldFormat: FieldFormat[A]) = {
     forAll(generator) { value =>
       val entity = createEntityWithField(fieldFormat, value)
       val roundTripped = fieldFormat.fromField(entity, fieldName)
@@ -155,7 +55,7 @@ trait FieldFormatTestCases extends GeneratorDrivenPropertyChecks with Matchers {
     }
   }
 
-  def testEntity[A](value: A)(assertion: Entity => Unit)(implicit fieldFormat: FieldFormat[A]) = {
+  private def testEntity[A](value: A)(assertion: Entity => Unit)(implicit fieldFormat: FieldFormat[A]) = {
     assertion(createEntityWithField(fieldFormat, value))
   }
 
